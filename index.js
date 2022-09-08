c8e97d4b846002eb89a9a63fbe00582f9eddd217
Marcel Cremer <marcel.cremer@mobiko.de>
2022-09-08 17:53:42 +0200

    Fixed authentication and list retrieval

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { createClient } from 'redis';
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+const authentication = process.env.REDIS_USER && process.env.REDIS_USER.trim() != '' ? `${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@` : ``;
+
 const client = createClient({
-  database: process.env.REDIS_DB,
-  username: process.env.REDIS_USER,
-  password: process.env.REDIS_PASSWORD,
+  url: `redis://${authentication}${process.env.REDIS_HOST}:${process.env.REDIS_PORT || 6379}/0`,
 });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
@@ -14,12 +14,15 @@ console.log('Trying to get a single job');
 
 await client.connect();
 
-const amount = await client.LLEN(REDIS_QUEUE);
+const amount = await client.LLEN(process.env.REDIS_LIST);
 if (amount == 0) {
   console.log('Nothing to do');
   process.exit(0);
 }
-const job = client.LPOP(offboarding);
+const job = await client.LPOP(process.env.REDIS_LIST);
 console.log(`Executing job with ${job}`);
+console.time();
 await delay(Math.floor(Math.random() * 10) * 1000);
 console.log(`Completed`);
+console.timeEnd();
+process.exit(0);
